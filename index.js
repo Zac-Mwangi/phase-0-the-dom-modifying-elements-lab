e9bcdd779e9c102c6ceeab0e2b79462207eba825
Zac-Mwangi <zackmwangi998@gmail.com>
2022-06-12 07:57:01 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+function removeMain() {
+  const main = document.getElementById("main");
+  main.remove();
+}
+
+removeMain();
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+// The innerText property would technically work as well; however, the tests won't pass if you use this approach
+newHeader.innerHTML = "YOUR-NAME is the champion";
+
